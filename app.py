c3019cabe5f8b4dadde45e836d67ef10152a6cf7
Lina-Arroyo <lina_arroyo@outlook.es>
2023-02-12 21:23:47 -0500

    Third  commit

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ st.markdown('This model predict the future sells for year in the ecommerce Olist
 
 st.header('Calculate the future Sells 💰')
 
-if st.button('Predict the future sells 👀'):
+if st.button('Predict the future sells 👀', key='unique'):
     st.markdown('These are the future sells for the next six months on Olist:')
     st.write(modelo.run_model(modelo.train, modelo.test, LinearRegression(), 'LinearRegression'))
