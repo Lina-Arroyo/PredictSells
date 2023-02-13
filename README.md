c14cb31d4dbe0919b06b5c023dea31ff819a647e
Lina-Arroyo <lina_arroyo@outlook.es>
2023-02-12 21:14:16 -0500

    Third  commit

---
+++ README.md
@@ -0,0 +1,6 @@
+$$ DEPLOY $$ 
+
+In this link you can acces the deploy Streamlit the prediction of the future sells in Olist
+
+http://localhost:8501/
+
--- app.py
+++ app.py
@@ -8,16 +8,6 @@ st.markdown('This model predict the future sells for year in the ecommerce Olist
 
 st.header('Calculate the future Sells 💰')
 
-#path = st.text_input('📥 Enter the CSV path to predict 📥', value="", max_chars=None, key=None, type="default",
-                      #help=None, autocomplete=None, on_change=None, 
-                      #args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible")
-
-#sep = st.text_input('📥 Enter the CSV separator  📥', value="", max_chars=None, key=None, type="default",
-                      #help=None, autocomplete=None, on_change=None, 
-                      #args=None, kwargs=None, placeholder=None, disabled=False, label_visibility="visible")
-
-
-
 if st.button('Predict the future sells 👀'):
     st.markdown('These are the future sells for the next six months on Olist:')
     st.write(modelo.run_model(modelo.train, modelo.test, LinearRegression(), 'LinearRegression'))
